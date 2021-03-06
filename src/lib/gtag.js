0f66b10f3d02e0079900dfa7480795889b2f2806
mashita1023 <coloring45@gmail.com>
2021-03-07 04:56:13 +0900

    fix google analytics

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -2,12 +2,12 @@ export const GA_TRACKING_ID = 'G-CQN52SV767'
 
 export const pageview = url => {
   window.gtag('config', GA_TRACKING_ID,
-    page_path: url,
+    page_path: url
   })
 }
 
 export const event = ({ action, category, label, value }) => {
-  windows.gtag('event', action, {
+    window.gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value
